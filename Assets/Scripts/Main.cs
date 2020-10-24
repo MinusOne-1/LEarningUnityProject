245ae28449e16f7cbe937c38e1b029d65579bdbe
пользоваель <Lannaig03@yandex.ru>
2020-10-24 18:17:59 +0300

    1

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Main : MonoBehaviour
 {
     // Start is called before the first frame update
+	[ReadOnly]
+	public bool generate = true;
+	[ReadOnly]
+	public int Massiv = 0;
+	
+	private Button button;
+	private XOGame gm;
+	
     void Start()
     {
-        
+        gm = GameObject.Find("Canvas").GetComponent<XOGame>();
+		button = gameObject.GetComponent<Button>();
+		button.onClick.AddListener(Starting);
     }
+	
+	public void Starting(){
+		gm.Paint(button);
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+ }
---
+++ Assets/Scripts/ReadOnlyAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+[AttributeUsage(AttributeTargets.Field, Inherited = true)]
+
+
+public class ReadOnlyAttribute : PropertyAttribute {  }
+
+#if UNITY_EDITOR
+public class ReadOnlyAttributeDrawer : UnityEditor.PropertyDrawer
+  {
+	
+	public override void OnGUI(Rect rect, UnityEditor.SerializedProperty prop, GUIContent label)
+		{
+			bool wasEnabled = GUI.enabled;
+			GUI.enabled = false;
+			UnityEditor.EditorGUI.PropertyField(rect, prop);
+			GUI.enabled = wasEnabled;
+			
+		}
+}
+#endif
